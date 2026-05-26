3dd267b9492877146d938b19094b17cbb3f19a07
Andreea-Topita <andreea-loredana.topita@student.tuiasi.ro>
2026-05-26 22:04:13 +0300

    Test mqtt flow: qos, last will, source_client_id

--- pc-monitor/src/infrastructure/database/connection.py
+++ pc-monitor/src/infrastructure/database/connection.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[3]
 load_dotenv(BASE_DIR / ".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
